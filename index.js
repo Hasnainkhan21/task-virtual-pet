84c4b0d6cd16c3f38eca6e99fd5eccc98f6ccdff
naino <Hasnainkhan21>
2024-10-13 00:17:04 +0500

    class made

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("hello gyus")
+var pet = {
+    name: "Cat",
+    age: 5,
+    happiness: 84,
+    hunger: 45,
+    feed: function() {
+        alert("the feeding is" + this.hunger - 20);
+    },
+
+    play: function() {
+        alert(this.happiness - 20)
+    },
+
+    agePet: function() {
+        alert(this.age +1 +"and" + thia.happiness - 5 + "and" + this.hunger + 10)
+    }
+};
+console.log(pet)
