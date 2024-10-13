2d48d22ac30c41131b248021c6e28d86cd949ce9
naino <Hasnainkhan21>
2024-10-13 23:48:58 +0500

    project done

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 var pet = {
     name: prompt("What is your pet name"),
-    type: prompt("what kind of pet is it"),
-    age: +prompt("how old is yor pet"),
-    happiness: +prompt("how happy is your pet"),
-    hunger: +prompt("how hungry is your pet"),
+    type: prompt("What kind of pet is it"),
+    age: +prompt("How old is your pet"),
+    happiness: +prompt("How happy is your pet"),
+    hunger: +prompt("How hungry is your pet"),
+    f: +prompt("What do you want to do with your pet \n 1.Feed \n 2.Play \n 3.Age \n 4.Quit"),
     feed: function() {
-        prompt("What do you want ro d with suzi \n 1.Feed \n 2.Play \n 3.Age \n 4.Quit")
-        
+        if(this.f === 1) {
+            this.hunger -= 20;
+            alert("Your hunger level is " + this.hunger);
+        }
     },
-
     play: function() {
-        alert(this.happiness - 20)
+        if(this.f === 2) {
+            this.happiness += 20;
+            alert("Your happiness increases up to " + this.happiness);
+        }
     },
-
     agePet: function() {
-        alert(this.age +1 +"and" + thia.happiness - 5 + "and" + this.hunger + 10)
+        if(this.f === 3) {
+            this.age += 1;
+            this.happiness -= 5;
+            this.hunger += 10;
+            alert("Your age is " + this.age + ", happiness is " + this.happiness + ", and hunger is " + this.hunger);
+        }
+    },
+    quite: function() {
+        if(this.f === 4) {
+            alert("Good bye, Thanks for taking care of " + this.name)
+        }
     }
 };
-console.log(pet)
-console.log(pet.feed());
+
+console.log(pet);
+pet.feed();
+pet.play();
+pet.agePet();
+pet.quite()
